b899f168657ae90828a3b11eef37e23247508298
Dr. Christian Betz <cb.betz@gmail.com>
2016-02-15 16:41:51 +0100

    uses the reconciler instead of the App component to perform updates to the state, thus making code easier to read.

--- src/cljs/kanban/components/boards_menu.cljs
+++ src/cljs/kanban/components/boards_menu.cljs
@@ -1,6 +1,8 @@
 (ns kanban.components.boards-menu
   (:require [om.next :as om :refer-macros [defui]]
-            [om.dom :as dom]))
+            [om.dom :as dom]
+            [kanban.reconciler :refer [reconciler]]
+            [kanban.parsing.boards :as boards]))
 
 (defui BoardMenuItem
   static om/Ident
@@ -12,27 +14,22 @@
   Object
   (render [this]
     (dom/li nil
-      (let [{:keys [name]} (om/props this)
-            {:keys [activate-fn]} (om/get-computed this)]
-        (dom/a #js {:onClick #(activate-fn (om/get-ident this))}
-          name)))))
+            (dom/a #js {:onClick #(om/transact! reconciler `[(boards/activate {:ref ~(om/get-ident this)})])}
+                   (:name (om/props this))))))
 
 (def board-menu-item (om/factory BoardMenuItem {:keyfn :id}))
 
 (defui BoardsMenu
   Object
   (render [this]
-    (let [{:keys [boards]} (om/props this)
-          {:keys [activate-fn create-fn]} (om/get-computed this)]
-      (dom/div #js {:className "header-menu"}
-        (dom/a nil "▾ Boards")
-        (dom/ul nil
-          (for [board boards]
-            (board-menu-item
-              (om/computed board {:activate-fn activate-fn})))
-          (dom/li #js {:className "header-menu-separator"})
-          (dom/li nil
-            (dom/a #js {:onClick create-fn}
-              "Create")))))))
+    (dom/div #js {:className "header-menu"}
+             (dom/a nil "▾ Boards")
+             (dom/ul nil
+                     (for [board (:boards (om/props this))]
+                       (board-menu-item board))
+                     (dom/li #js {:className "header-menu-separator"})
+                     (dom/li nil
+                             (dom/a #js {:onClick #(om/transact! reconciler `[(boards/create-board)])}
+                                    "Create"))))))
 
 (def boards-menu (om/factory BoardsMenu))
